08ad0be8bca2d1c24d1916de65fbd0985377211a
Max Bachmann <kontakt@maxbachmann.de>
2021-09-22 15:15:09 +0200

    Update intrinsics.hpp

--- rapidfuzz/details/intrinsics.hpp
+++ rapidfuzz/details/intrinsics.hpp
@@ -5,17 +5,27 @@
 namespace rapidfuzz {
 namespace intrinsics {
 
-#include <rapidfuzz/details/builtin.h>
-
 static inline uint64_t addc64(uint64_t a, uint64_t b, uint64_t carryin, uint64_t* carryout)
 {
     /* todo should use _addcarry_u64 when available */
-    return psnip_builtin_addc64(a, b, carryin, carryout);
+    a += carryin;
+    *carryout = a < carryin;
+    a += b;
+    *carryout |= a < b;
+    return a;
 }
 
 static inline std::size_t popcount64(uint64_t x)
 {
-    return psnip_builtin_popcount64(x);
+    const uint64_t m1 = 0x5555555555555555;
+    const uint64_t m2 = 0x3333333333333333;
+    const uint64_t m4 = 0x0f0f0f0f0f0f0f0f;
+    const uint64_t h01 = 0x0101010101010101;
+
+    x -= (x >> 1) & m1;
+    x = (x & m2) + ((x >> 2) & m2);
+    x = (x + (x >> 4)) & m4;
+    return static_cast<std::size_t>((x * h01) >> 56);
 }
     
 } // namespace intrinsics
