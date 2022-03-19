5f450841d26e15b3396ebeffe345c9534e48a918
Max Bachmann <kontakt@maxbachmann.de>
2022-03-19 14:03:45 +0100

    add LCS distance (#63)

---
+++ rapidfuzz/distance/LCSseq.hpp
@@ -0,0 +1,101 @@
+/* SPDX-License-Identifier: MIT */
+/* Copyright © 2022-present Max Bachmann */
+
+#pragma once
+#include <rapidfuzz/details/common.hpp>
+
+#include <cmath>
+#include <limits>
+
+namespace rapidfuzz {
+
+template <typename InputIt1, typename InputIt2>
+int64_t lcs_seq_distance(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2,
+                         int64_t max = std::numeric_limits<int64_t>::max());
+
+template <typename Sentence1, typename Sentence2>
+int64_t lcs_seq_distance(const Sentence1& s1, const Sentence2& s2,
+                         int64_t max = std::numeric_limits<int64_t>::max());
+
+template <typename InputIt1, typename InputIt2>
+int64_t lcs_seq_similarity(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2,
+                           int64_t score_cutoff = 0);
+
+template <typename Sentence1, typename Sentence2>
+int64_t lcs_seq_similarity(const Sentence1& s1, const Sentence2& s2, int64_t score_cutoff = 0);
+
+template <typename InputIt1, typename InputIt2>
+double lcs_seq_normalized_distance(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2,
+                                   double score_cutoff = 1.0);
+
+template <typename Sentence1, typename Sentence2>
+double lcs_seq_normalized_distance(const Sentence1& s1, const Sentence2& s2,
+                                   double score_cutoff = 1.0);
+
+template <typename InputIt1, typename InputIt2>
+double lcs_seq_normalized_similarity(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+                                     InputIt2 last2, double score_cutoff = 0.0);
+
+template <typename Sentence1, typename Sentence2>
+double lcs_seq_normalized_similarity(const Sentence1& s1, const Sentence2& s2,
+                                     double score_cutoff = 0.0);
+
+template <typename InputIt1, typename InputIt2>
+Editops lcs_seq_editops(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2);
+
+template <typename Sentence1, typename Sentence2>
+Editops lcs_seq_editops(const Sentence1& s1, const Sentence2& s2);
+
+template <typename CharT1>
+struct CachedLCSseq {
+    template <typename Sentence1>
+    CachedLCSseq(const Sentence1& s1_)
+        : s1(common::to_string(s1_)), PM(common::to_begin(s1), common::to_end(s1))
+    {}
+
+    template <typename InputIt1>
+    CachedLCSseq(InputIt1 first1, InputIt1 last1) : s1(first1, last1), PM(first1, last1)
+    {}
+
+    template <typename InputIt2>
+    int64_t distance(InputIt2 first2, InputIt2 last2,
+                     int64_t score_cutoff = std::numeric_limits<int64_t>::max()) const;
+
+    template <typename Sentence2>
+    int64_t distance(const Sentence2& s2,
+                     int64_t score_cutoff = std::numeric_limits<int64_t>::max()) const;
+
+    template <typename InputIt2>
+    int64_t similarity(InputIt2 first2, InputIt2 last2, int64_t score_cutoff = 0) const;
+
+    template <typename Sentence2>
+    int64_t similarity(const Sentence2& s2, int64_t score_cutoff = 0) const;
+
+    template <typename InputIt2>
+    double normalized_distance(InputIt2 first2, InputIt2 last2, double score_cutoff = 1.0) const;
+
+    template <typename Sentence2>
+    double normalized_distance(const Sentence2& s2, double score_cutoff = 1.0) const;
+
+    template <typename InputIt2>
+    double normalized_similarity(InputIt2 first2, InputIt2 last2, double score_cutoff = 0.0) const;
+
+    template <typename Sentence2>
+    double normalized_similarity(const Sentence2& s2, double score_cutoff = 0.0) const;
+
+private:
+    std::basic_string<CharT1> s1;
+    common::BlockPatternMatchVector PM;
+};
+
+#if __cplusplus >= 201703L
+template <typename Sentence1>
+CachedLCSseq(const Sentence1& s1_) -> CachedLCSseq<char_type<Sentence1>>;
+
+template <typename InputIt1>
+CachedLCSseq(InputIt1 first1, InputIt1 last1) -> CachedLCSseq<iterator_type<InputIt1>>;
+#endif
+
+} // namespace rapidfuzz
+
+#include <rapidfuzz/distance/LCSseq.impl>
---
+++ test/distance/tests-LCSseq.cpp
@@ -0,0 +1,48 @@
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+
+#include <rapidfuzz/distance/Indel.hpp>
+
+TEST_CASE("Indel")
+{
+    std::string test = "aaaa";
+    std::string replace_all = "bbbb";
+
+    SECTION("similar strings")
+    {
+        REQUIRE(rapidfuzz::lcs_seq_distance(test, test) == 0);
+        REQUIRE(rapidfuzz::lcs_seq_similarity(test, test) == 4);
+        REQUIRE(rapidfuzz::lcs_seq_normalized_distance(test, test) == 0.0);
+        REQUIRE(rapidfuzz::lcs_seq_normalized_similarity(test, test) == 1.0);
+    }
+
+    SECTION("completly different strings")
+    {
+        REQUIRE(rapidfuzz::lcs_seq_distance(test, replace_all) == 4);
+        REQUIRE(rapidfuzz::lcs_seq_similarity(test, replace_all) == 0);
+        REQUIRE(rapidfuzz::lcs_seq_normalized_distance(test, replace_all) == 1.0);
+        REQUIRE(rapidfuzz::lcs_seq_normalized_similarity(test, replace_all) == 0.0);
+    }
+
+    SECTION("some tests for mbleven")
+    {
+        std::string a = "South Korea";
+        std::string b = "North Korea";
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b) == 2);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 4) == 2);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 3) == 2);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 2) == 2);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 1) == 2);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 0) == 1);
+
+        a = "aabc";
+        b = "cccd";
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b) == 3);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 4) == 3);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 3) == 3);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 2) == 3);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 1) == 2);
+        REQUIRE(rapidfuzz::lcs_seq_distance(a, b, 0) == 1);
+    }
+};
