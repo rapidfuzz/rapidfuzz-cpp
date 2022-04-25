b72427573f99c51968bdd8daec981049b74f605b
Max Bachmann <kontakt@maxbachmann.de>
2022-04-25 14:55:14 +0200

    use ceil_div

--- rapidfuzz/details/common.hpp
+++ rapidfuzz/details/common.hpp
@@ -9,7 +9,7 @@
 #include <rapidfuzz/details/SplittedSentenceView.hpp>
 #include <rapidfuzz/details/type_traits.hpp>
 #include <rapidfuzz/details/types.hpp>
-#include <tuple>
+#include <rapidfuzz/details/intrinsics.hpp>
 #include <unordered_set>
 #include <vector>
 
@@ -271,7 +271,7 @@ struct BlockPatternMatchVector {
     void insert(InputIt first, InputIt last)
     {
         auto len = std::distance(first, last);
-        auto block_count = len / 64 + bool(len % 64);
+        auto block_count = ceil_div(len, 64);
         m_val.resize(static_cast<size_t>(block_count));
 
         for (ptrdiff_t block = 0; block < block_count; ++block) {
