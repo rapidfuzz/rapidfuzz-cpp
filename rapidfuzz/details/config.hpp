3bfc9c6c3046c0d2539d5aa94c14b51dd239dc9c
Max Bachmann <oss@maxbachmann.de>
2025-02-11 13:40:55 +0100

    fix compilation on old msvc versions

--- rapidfuzz/details/config.hpp
+++ rapidfuzz/details/config.hpp
@@ -5,6 +5,17 @@
 
 #if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
 #    define RAPIDFUZZ_DEDUCTION_GUIDES
+#endif
+
+/* older versions of msvc have bugs in their if constexpr support
+ * see https://github.com/rapidfuzz/rapidfuzz-cpp/issues/122
+ * since we don't know the exact version this was fixed in, use the earliest we could test
+ */
+#if defined(_MSC_VER) && _MSC_VER < 1920
+#    define RAPIDFUZZ_IF_CONSTEXPR_AVAILABLE 0
+#    define RAPIDFUZZ_IF_CONSTEXPR if
+#elif ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
+#    define RAPIDFUZZ_DEDUCTION_GUIDES
 #    define RAPIDFUZZ_IF_CONSTEXPR_AVAILABLE 1
 #    define RAPIDFUZZ_IF_CONSTEXPR if constexpr
 #else
